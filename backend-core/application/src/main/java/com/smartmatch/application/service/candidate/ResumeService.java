2aa41db7a585dac416b5271295ac1fefbc31dab1
Julian Nguyen <huutrong.160705@gmail.com>
2026-04-05 21:12:39 +0700

    add IOException

--- backend-core/application/src/main/java/com/smartmatch/application/service/candidate/ResumeService.java
+++ backend-core/application/src/main/java/com/smartmatch/application/service/candidate/ResumeService.java
@@ -2,9 +2,11 @@ package com.smartmatch.application.service.candidate;
 
 import com.smartmatch.application.dto.candidate.ResumeResponse;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 import java.util.List;
 
 public interface ResumeService {
-    ResumeResponse uploadResume(MultipartFile file, Long candidateId);
+    ResumeResponse uploadResume(MultipartFile file, Long candidateId) throws IOException;
     List<ResumeResponse> getMyResumes(Long candidateId);
 }
